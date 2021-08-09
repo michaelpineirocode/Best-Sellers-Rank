e35baca1f2a63522797726cf2a1488e179668be2
Michael Pineiro <68714417+michaelpineirocode@users.noreply.github.com>
2021-08-09 16:39:55 -0600

    Create settings.py

---
+++ src/settings.py
@@ -0,0 +1,20 @@
+# URL of the website
+url = "https://www.amazon.com/Noble-Ultimatum-Jack-Thrillers-Book-ebook/dp/B08K7FXKSF/ref=sr_1_1?dchild=1&keywords=noble+ultimatum&qid=1628540825&sr=8-1"
+
+# The name of the json directory
+json_title = "api-key.json"
+
+# Title of the spreadsheet
+spreadsheet_title = "testin"
+
+# Sheet NUMBER of what needs to be changed
+sheet_number = 0 # 0 is the first sheet
+
+# Whether or not the spot will replace an existing cell or add to a row
+replace = False
+
+# Choose whether to add the data to a column or to a row
+by_column = True
+
+# Which column/row to add the new info to
+line_num = 1
